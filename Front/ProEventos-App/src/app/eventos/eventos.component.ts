afe454959c94c273fe081d1b462703296e58ff82
KLEBER_ALENCAR <kleber.alencar@outlook.com>
2023-03-07 14:19:16 -0300

    Refactoring code

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -4,11 +4,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
 
-  public eventos: any;
+  larguraImagem: number = 150;
+  margemImagem: number = 2;
+  mostrarImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -16,11 +41,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-  public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    );
+  alterarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
+  public getEventos(): void {
+    this.http.get('http://localhost:5001/api/eventos').subscribe(
+      (response) => {
+        (this.eventos = response);
+        this.eventosFiltrados = this.eventos;
+      },
+      (error) => console.log(error)
+    );
+  }
 }
